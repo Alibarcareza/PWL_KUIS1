6f39c24e7bf178e041a3ba8cf4e9482984a60844
tzelginianc <tzelginia78@gmail.com>
2022-03-22 14:51:19 +0700

    edit barang

--- resources/views/barang.blade.php
+++ resources/views/barang.blade.php
@@ -7,8 +7,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-heading">
-                        <h2>Our Latest Products</h2>
-                        <span>Check out all of our products.</span>
+                        <h2>Daftar Produk Kami</h2>
+                        <span>Selamat datang di daftar produk kami.</span>
                     </div>
                 </div>
             </div>
