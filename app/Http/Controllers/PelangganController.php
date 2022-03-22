0d26a2cc6df5a70dd29226b99dc50b30477ba51e
tzelginianc <tzelginia78@gmail.com>
2022-03-22 14:45:19 +0700

    paginate pelanggan

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -7,7 +7,8 @@ use App\Models\pelanggan;
 class PelangganController extends Controller
 {
     public function pelanggan(){
-        $allpelanggan=pelanggan::all();
+        // $allpelanggan=pelanggan::all();
+        $allpelanggan=pelanggan::paginate(1);
         return view('pelanggan')
         ->with('title','Pelanggan')
         ->with('all_pelanggan',$allpelanggan);
